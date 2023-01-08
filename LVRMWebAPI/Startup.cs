3a5c6e6e6ab11ab3643175f4a574cee84fc5e13c
Suresh Tripathi <Suresh.Tripathi@leadventure.com>
2023-01-08 11:56:36 +0530

    Test

--- LVRMWebAPI/Startup.cs
+++ LVRMWebAPI/Startup.cs
@@ -32,7 +32,7 @@ namespace LVRMWebAPI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // Test Suresh
+        // Test Suresh Tripathi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IScopedSevices, MyScopServices>();
